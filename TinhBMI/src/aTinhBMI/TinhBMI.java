07cd0e3998ca7e9bff7d6b0a97174f42ef92b14e
Anh <nguyenanh20191@gmail.com>
2024-09-24 20:13:06 +0700

    Nhập được 1 ít

--- TinhBMI/src/aTinhBMI/TinhBMI.java
+++ TinhBMI/src/aTinhBMI/TinhBMI.java
@@ -3,8 +3,17 @@ package aTinhBMI;
 public class TinhBMI {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner banPhim = new Scanner (System.in);
+		// Nhập họ tên
+		System.out.print("Họ tên: ");
+		String hoTen= banPhim.nextLine();
+		//Nhập chiều cao
+		System.out.print("Chiều cao (m): ");
+		double chieuCao = banPhim.nextDouble();
+		//Nhập cân nặng
+		System.out.print("Cân nặng (kg):");
+		double canNang = banPhim.nextDouble();
+		
 	}
 
 }
