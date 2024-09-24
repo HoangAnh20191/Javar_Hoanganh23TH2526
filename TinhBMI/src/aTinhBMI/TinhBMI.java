21a599e5e13f55b25cdfc7712f69e3eb7e208b5f
Anh <nguyenanh20191@gmail.com>
2024-09-24 20:24:16 +0700

    Đưa ra nhận xét

--- TinhBMI/src/aTinhBMI/TinhBMI.java
+++ TinhBMI/src/aTinhBMI/TinhBMI.java
@@ -22,6 +22,19 @@ public class TinhBMI {
 		System.out.println("Bạn " + hoTen);
 		System.out.println("Có chỉ số BMI = " + bmi);
 		
+		//Đưa ra nhận xét
+		if (bmi<18.5)
+			System.out.println("Bạn thiếu cân, cần bổ sung thêm nhiều dinh dưỡng");
+		else if(bmi<25)
+			System.out.println("Chúc mừng, bạn có cân nặng bình thường");
+		else if(bmi<30)
+			System.out.println("Bạn bị tiền béo phì, cân đối lại chế độ ăn uống nhé!");
+		else if(bmi<35)
+			System.out.println("Bạn bị béo phì độ 1, bạn cần tập thể dục nhiều hơn");
+		else if(bmi<40)
+			System.out.println("Bạn bị béo phì độ 2, bạn cần kiểm soát năng lượng nạp vào và tập thể dục nhiều hơn");
+		else System.out.println("Bạn bị béo phì độ 3, cần đi đến chuyên gia!!!");
+		
 	}
 
 }
