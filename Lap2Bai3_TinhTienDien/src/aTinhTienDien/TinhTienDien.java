406c60c0cd6ad85ef65f4c8b59d98cdc6c45dc50
Anh <nguyenanh20191@gmail.com>
2024-09-27 13:18:30 +0700

    Hoàn thiện bài tính tiền điện

--- Lap2Bai3_TinhTienDien/src/aTinhTienDien/TinhTienDien.java
+++ Lap2Bai3_TinhTienDien/src/aTinhTienDien/TinhTienDien.java
@@ -6,11 +6,20 @@ public class TinhTienDien {
 
 	public static void main(String[] args) {
 		Scanner abc = new Scanner(System.in);
-		
-		// Nhập số tiền điện tiêu thụ
-		System.out.println("Số kW điện tiêu thụ: ");
+		System.out.println("Bài toán tính số tiền điện");
+		System.out.println("-------------------------------");
+		// Nhập số kW điện tiêu thụ
+		System.out.println("Nhập số kW điện tiêu thụ: ");
 		Double kW = abc.nextDouble();
 		
+		//Tính số tiền điện tiêu thụ
+		if (kW < 50){
+			Double tienDien = kW*1000;
+			System.out.println("Số tiền điện phải trả = " + tienDien + " đồng");
+		}else {
+			Double tienDien = kW*1000 + (kW-50)*1200;
+			System.out.println("Số tiền điện phải trả = " + tienDien + " đồng)");
+		}
 
 	}
 
