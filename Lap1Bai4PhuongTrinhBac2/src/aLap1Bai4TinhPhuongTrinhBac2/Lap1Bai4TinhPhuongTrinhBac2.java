0390e7e796ae6704d74f6c8bf4aa42275adb3694
Anh <nguyenanh20191@gmail.com>
2024-09-27 08:10:00 +0700

    TÍnh delta

--- Lap1Bai4PhuongTrinhBac2/src/aLap1Bai4TinhPhuongTrinhBac2/Lap1Bai4TinhPhuongTrinhBac2.java
+++ Lap1Bai4PhuongTrinhBac2/src/aLap1Bai4TinhPhuongTrinhBac2/Lap1Bai4TinhPhuongTrinhBac2.java
@@ -7,6 +7,7 @@ public class Lap1Bai4TinhPhuongTrinhBac2 {
 	public static void main(String[] args) {
 		Scanner PT = new Scanner (System.in);
 		System.out.println("Phương trình bậc 2: ax^2 + bx + c = 0 ");
+		System.out.println("----------------------");
 		// Nhập vào các hệ số a,b,c
 		System.out.println("Nhập hệ số a: ");
 		double a = PT.nextDouble();
@@ -14,7 +15,10 @@ public class Lap1Bai4TinhPhuongTrinhBac2 {
 		double b = PT.nextDouble();
 		System.out.println("Nhập hệ số c: ");
 		double c = PT.nextDouble();
-
+		//Tính delta
+		double delta = Math.pow(b, 2) - 4*a*c;
+		//In ra kết quả
+		System.out.println("Denta = " + delta);
 	}
 
 }
