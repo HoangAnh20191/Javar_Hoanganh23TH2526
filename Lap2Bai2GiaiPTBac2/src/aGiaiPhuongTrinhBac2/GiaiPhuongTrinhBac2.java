edc024f9944896a0e9e99646271757e192c5762b
Anh <nguyenanh20191@gmail.com>
2024-09-27 12:59:15 +0700

    Hoàn chỉnh giải phương trình bậc 2

--- Lap2Bai2GiaiPTBac2/src/aGiaiPhuongTrinhBac2/GiaiPhuongTrinhBac2.java
+++ Lap2Bai2GiaiPTBac2/src/aGiaiPhuongTrinhBac2/GiaiPhuongTrinhBac2.java
@@ -6,7 +6,7 @@ public class GiaiPhuongTrinhBac2 {
 
 	public static void main(String[] args) {
 		Scanner abc = new Scanner(System.in);
-		
+		System.out.println("-----Giải phương trình bậc 2-----");
 		// Nhập các hệ số a, b, c (ax^2 + bx + c = 0)
 		System.out.println("Nhập hệ số a :");
 		double a = abc.nextDouble();
@@ -16,6 +16,7 @@ public class GiaiPhuongTrinhBac2 {
 		double c = abc.nextDouble();
 		
 		//Giải phương trình
+	// Trường hợp 1: a = 0
 		if (a==0) {
 			//Giải phương trình bậc 1 bx + c = 0
 			if (b==0) {
@@ -30,6 +31,25 @@ public class GiaiPhuongTrinhBac2 {
 				double x = -c/b;
 				System.out.println("Phương trình có nghiệm x = " + x);
 			}
+		}
+	
+		else {
+			//Trường hợp 2: a khác 0 , tính denta
+			double denta = b*b -4*a*c;
+			if (denta <0) {
+				System.out.println("Phương trình vô nghiệm");
+			} else
+				if (denta ==0) {
+					double x = -b/2*a;
+					System.out.println("Phương trình có nghiệm kép x = " + x);
+				} else {
+					double canDenta = Math.sqrt(denta);
+					double x1 = ((-b+canDenta)/2*a);
+					double x2 = ((-b-canDenta)/2*a);
+					System.out.println("Phương trình có 2 nghiệm: ");
+					System.out.println("x1 = " + x1);
+					System.out.println("x2 = " + x2);
+				}
 			
 		}
 		
