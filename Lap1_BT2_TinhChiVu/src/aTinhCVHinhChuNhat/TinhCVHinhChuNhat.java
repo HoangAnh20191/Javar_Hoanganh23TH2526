7b72f817312d2bcd055a286dbfe347a66e628c4f
Anh <nguyenanh20191@gmail.com>
2024-09-27 07:18:52 +0700

    Nhập chiều dài, rộng

--- Lap1_BT2_TinhChiVu/src/aTinhCVHinhChuNhat/TinhCVHinhChuNhat.java
+++ Lap1_BT2_TinhChiVu/src/aTinhCVHinhChuNhat/TinhCVHinhChuNhat.java
@@ -1,9 +1,19 @@
 package aTinhCVHinhChuNhat;
 
+import java.util.Scanner;
+
 public class TinhCVHinhChuNhat {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner a = new Scanner (System.in);
+		System.out.println("--------Tính chi vi hình chữ nhật--------");
+		// Nhập chiều dài và chiều rộng
+		System.out.println("Nhập chiều dài: ");
+		double chieuDai = a.nextDouble();
+		System.out.println("Nhập chiều rộng: ");
+		double chieuRong = a.nextDouble();
+		
 
 	}
 
