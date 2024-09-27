9efaebdf825749b484e54f87326b4b3d38f2fe9b
Anh <nguyenanh20191@gmail.com>
2024-09-27 12:25:59 +0700

    Hoàn chỉnh PT bậc 1

--- Lap2Bai1PhuongTrinhBac1/src/aGiaiPhuongTrinhBac1/GiaiPhuongTrinhBac1.java
+++ Lap2Bai1PhuongTrinhBac1/src/aGiaiPhuongTrinhBac1/GiaiPhuongTrinhBac1.java
@@ -1,9 +1,31 @@
 package aGiaiPhuongTrinhBac1;
 
+import java.util.Scanner;
+
 public class GiaiPhuongTrinhBac1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner abc = new Scanner(System.in);
+		
+		// Nhập hệ số a,b (phương trình bậc 1: a*x + b = 0)
+		System.out.println("Nhập hệ số a: ");
+		double a = abc.nextDouble();
+		System.out.println("Nhập hệ số b: ");
+		double b =abc.nextDouble();
+		
+		//Giải phương trình
+		if (a==0) {
+			if(b==0) {
+				System.out.println("Phương trình vô số nghiệm");
+			}
+			else {
+				System.out.println("Phương trình vô nghiệm");
+			}
+		}
+		else {
+			double x = -b / a;
+			System.out.println("Phương trình có nghiệm x = " + x);
+		}
 
 	}
 
