6aa6f4e6f273a24456c4466834fb67062e28f2b1
Anh <nguyenanh20191@gmail.com>
2024-09-27 10:20:20 +0700

    Nhập bán kính

--- TinhChuViHinhTron/src/aTinhCVHinhTron/TinhCVHinhTron.java
+++ TinhChuViHinhTron/src/aTinhCVHinhTron/TinhCVHinhTron.java
@@ -1,9 +1,23 @@
 package aTinhCVHinhTron;
 
+import java.util.Scanner;
+
 public class TinhCVHinhTron {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner HT = new Scanner(System.in);
+		final double PI = 3.141159;
+		
+		// Nhập bán kính hình tròn
+		System.out.println("Nhập bán kính hình tròn: ");
+		double banKinh = HT.nextDouble();
+		
+		//Tính chu vi
+		double chuVi = 2*PI*banKinh;
+		System.out.println("Chu vi hình tròn = " + chuVi);
+		
+		//Tính diện tích
+		double dienTich = PI*banKinh*banKinh;
 
 	}
 
