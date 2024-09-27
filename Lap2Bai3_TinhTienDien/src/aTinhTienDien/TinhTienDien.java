c56775eef5c5460493d28e37e302bfb30e6043fc
Anh <nguyenanh20191@gmail.com>
2024-09-27 13:05:19 +0700

    Nhập số kW điện tiêu thụ

---
+++ Lap2Bai3_TinhTienDien/src/aTinhTienDien/TinhTienDien.java
@@ -0,0 +1,17 @@
+package aTinhTienDien;
+
+import java.util.Scanner;
+
+public class TinhTienDien {
+
+	public static void main(String[] args) {
+		Scanner abc = new Scanner(System.in);
+		
+		// Nhập số tiền điện tiêu thụ
+		System.out.println("Số kW điện tiêu thụ: ");
+		Double kW = abc.nextDouble();
+		
+
+	}
+
+}
