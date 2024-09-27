f8478c3259d5b1d523d1bd796d8c06930dc5b7cd
Anh <nguyenanh20191@gmail.com>
2024-09-27 08:07:38 +0700

    Nhập số abc

---
+++ Lap1Bai4PhuongTrinhBac2/src/aLap1Bai4TinhPhuongTrinhBac2/Lap1Bai4TinhPhuongTrinhBac2.java
@@ -0,0 +1,20 @@
+package aLap1Bai4TinhPhuongTrinhBac2;
+
+import java.util.Scanner;
+
+public class Lap1Bai4TinhPhuongTrinhBac2 {
+
+	public static void main(String[] args) {
+		Scanner PT = new Scanner (System.in);
+		System.out.println("Phương trình bậc 2: ax^2 + bx + c = 0 ");
+		// Nhập vào các hệ số a,b,c
+		System.out.println("Nhập hệ số a: ");
+		double a = PT.nextDouble();
+		System.out.println("Nhập hệ số b: ");
+		double b = PT.nextDouble();
+		System.out.println("Nhập hệ số c: ");
+		double c = PT.nextDouble();
+
+	}
+
+}
